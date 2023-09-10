74bbcb0c81e908d3d9b0c30cb2e10f7bda7b8709
Tosyanchyk <ddd.tosha@gmail.com>
2023-09-10 22:47:59 +0300

    adding some handles of errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -10,15 +10,21 @@ const AvailableMeals = () => {
 
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);//not for all cases just for that
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
-    const fetchMeals = async() => {
+    const fetchMeals = async () => {
       const response = await fetch('https://food-order-app-a4890-default-rtdb.firebaseio.com/Meals.json'); //fetch returns a promise
+
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
 
-      for(const key in responseData){
+      for (const key in responseData) {
         loadedMeals.push({
           id: key,
           name: responseData[key].name,
@@ -30,32 +36,43 @@ const AvailableMeals = () => {
       setMeals(loadedMeals);
       setIsLoading(false);
     };
-    fetchMeals();
-}, []);
 
-if (isLoading){
-  return <section className={s.MealsLoading}>
-    <p>LOADING...</p>
-  </section>
-}
+    
+     fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);//handling error inside of a promise
+     });
+  }, []);
 
-const mealsList = meals.map((meal) => (
-  <MealItem
-    key={meal.id}
-    id={meal.id}
-    name={meal.name}
-    description={meal.description}
-    price={meal.price}
-  />
-));
+  if (isLoading) {
+    return <section className={s.MealsLoading}>
+      <p>LOADING...</p>
+    </section>
+  }
 
-return (
-  <section className={s.meals}>
-    <Card>
-      <ul>{mealsList}</ul>
-    </Card>
-  </section>
-);
+  if (httpError) {
+    return <section className={s.MealsError}>
+      <p>{httpError}</p>
+    </section>
+  }
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={s.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
 };
 
 export default AvailableMeals;
